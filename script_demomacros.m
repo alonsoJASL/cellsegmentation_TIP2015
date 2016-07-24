4b8f54f2475ae007cf49490a05514be0537e2980
Jose Alonso Solis Lemus <alonso.jasl@gmail.com>
2016-07-25 00:23:13 +0100

    MACROPHAGES script added

---
+++ script_demomacros.m
@@ -0,0 +1,21 @@
+% Script file: demo - Macrophages dataset
+%
+% 
+
+%% Initialisation OSX 
+
+plat = 'osx';
+[dn,ds] = loadnames('macros', plat);
+names = [55 75 95];
+
+name = 'man00';
+
+%% Initialisation Linux
+
+plat = 'linux';
+[dn,ds] = loadnames('macros', plat);
+names = [55 75 95];
+
+name = 'man00';
+
+%% 
